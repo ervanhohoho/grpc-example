204305635cd2462beb55256145801b52e73a3514
Ervan Dharmawan <ervandharmawanhalim@gmail.com>
2023-08-09 17:14:06 +0700

    fix client code

--- generate-from-proto.sh
+++ generate-from-proto.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-protoc --go_out=. --go-grpc_out=. "helloworld.proto"
+protoc --go_out=pb_gen --go_opt=paths=source_relative --go-grpc_out=pb_gen --go-grpc_opt=paths=source_relative "helloworld.proto"
