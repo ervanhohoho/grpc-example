0049836c762b4931d81193f19880af2a2b2df58b
Ervan Dharmawan <ervandharmawanhalim@gmail.com>
2023-08-09 17:30:06 +0700

    add server.js

--- generate-from-proto.sh
+++ generate-from-proto.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-protoc --go_out=pb_gen --go_opt=paths=source_relative --go-grpc_out=pb_gen --go-grpc_opt=paths=source_relative "helloworld.proto"
+protoc  --go_out=pb_gen --go_opt=paths=source_relative \
+           --go-grpc_out=pb_gen --go-grpc_opt=paths=source_relative "helloworld.proto"
